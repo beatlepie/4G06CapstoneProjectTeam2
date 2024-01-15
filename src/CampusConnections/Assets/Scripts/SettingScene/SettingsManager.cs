59d583801aa222b6fc9b912fea80e28121cb3abf
beetlepie <minsung0816@gmail.com>
2024-01-14 22:31:06 -0500

    feat(#148) change password done

--- src/CampusConnections/Assets/Scripts/SettingScene/SettingsManager.cs
+++ src/CampusConnections/Assets/Scripts/SettingScene/SettingsManager.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 using Firebase.Database;
 using Firebase.Auth;
 using TMPro;
+using System.Collections;
 
 public class SettingsManager : MonoBehaviour
 {
@@ -43,11 +44,13 @@ public class SettingsManager : MonoBehaviour
     private void Start()
     {
         // This value should be the query user value when navigated from friend!
-        FirebaseUser query = auth.CurrentUser;
         auth = FirebaseAuth.DefaultInstance;
+        FirebaseUser query = auth.CurrentUser;
         db = FirebaseDatabase.DefaultInstance.RootReference;
         // Display is the default view
+        DisplayCanvas.SetActive(true);
         EditCanvas.SetActive(false);
+        PasswordCanvas.SetActive(false);
         // If the id is not the user, then remove the edit button!
         if (auth.CurrentUser != query)
         {
@@ -73,9 +76,15 @@ public class SettingsManager : MonoBehaviour
     /// <summary>
     /// Function for calling values from the database.
     /// </summary>
-    private void getDBdata(FirebaseUser query)
+    private IEnumerator getDBdata(FirebaseUser query)
     {
-        
+        var value = db.Child("Users").Child(query.Email).GetValueAsync();
+        userIDDisplay.text = value.Result.Value.ToString();
+
+        DataSnapshot item = value.Result;
+        level.text = item.Child("Level").Value.ToString();
+
+        yield return new WaitUntil(predicate: () => value.IsCompleted);
     }
 
     /// <summary>
@@ -102,21 +111,50 @@ public class SettingsManager : MonoBehaviour
     /// </summary>
     public void SaveNewPassword()
     {
-        if(CurrentPassword.text == auth.CurrentUser.ToString())
-        {
-            if(NewPassword.text == ConfirmPassword.text)
-            {
+        FirebaseUser user = auth.CurrentUser;
 
+        // This checks that the current password
+        user.ReauthenticateAsync(EmailAuthProvider.GetCredential(user.Email, CurrentPassword.text)).ContinueWith(task =>
+        {
+            Debug.LogFormat("new password attempt!");
+            // If the current password is correct:
+            if (task.IsCompletedSuccessfully)
+            {
+                Debug.LogFormat("Password correct!");
+                // If the new passwords match:
+                if (NewPassword.text == ConfirmPassword.text)
+                {
+                    Debug.LogFormat("Password matches!");
+                    // This attempts to update the password
+                    user.UpdatePasswordAsync(NewPassword.text).ContinueWith(update => 
+                    {
+                        if (update.IsCompletedSuccessfully)
+                        {
+                            Debug.LogFormat("new password updated!");
+                            status.text = "New password update successful!";
+                        }
+                        else
+                        {
+                            Debug.LogFormat(update.Exception.ToString());
+                            Debug.LogFormat("new password update failed!");
+                            // There is a chance insecure password is used, firebase will reject that password
+                            status.text = "New password update failed! \n" +
+                                          "This password may not be viable!";
+                        }
+                        return;
+                    });
+                }
+                else
+                {
+                    status.text = "New password does not match!";
+                }
             }
             else
             {
-
+                status.text = "Current password is incorrect!";
             }
-        }
-        else
-        {
-            status.text = "Current password is incorrect!";
-        }
+            return;
+        });
     }
 
     /// <summary>
