5a4cbd1095bbddfe8baa4d8918179f38d4b6daa1
beetlepie <minsung0816@gmail.com>
2024-01-17 04:00:03 -0500

    feat(#148): profile page complete for now

--- src/CampusConnections/Assets/Scripts/SettingScene/SettingsManager.cs
+++ src/CampusConnections/Assets/Scripts/SettingScene/SettingsManager.cs
@@ -4,6 +4,8 @@ using Firebase.Database;
 using Firebase.Auth;
 using TMPro;
 using System.Collections;
+using System;
+using System.Collections.Generic;
 
 public class SettingsManager : MonoBehaviour
 {
@@ -11,6 +13,7 @@ public class SettingsManager : MonoBehaviour
 
     private FirebaseAuth auth;
     private DatabaseReference db;
+    private FirebaseUser query;
 
     [Header("Canvas")]
     // Canvas containing user information
@@ -36,6 +39,8 @@ public class SettingsManager : MonoBehaviour
 
     [Header("Input Values")]
     public TMP_InputField newUsername;
+    public TMP_InputField newLevel;
+    public TMP_InputField newProgram;
     // These values are used during change password
     public TMP_InputField CurrentPassword;
     public TMP_InputField NewPassword;
@@ -45,7 +50,7 @@ public class SettingsManager : MonoBehaviour
     {
         // This value should be the query user value when navigated from friend!
         auth = FirebaseAuth.DefaultInstance;
-        FirebaseUser query = auth.CurrentUser;
+        query = auth.CurrentUser;
         db = FirebaseDatabase.DefaultInstance.RootReference;
         // Display is the default view
         DisplayCanvas.SetActive(true);
@@ -57,7 +62,14 @@ public class SettingsManager : MonoBehaviour
             EditButton.SetActive(false);
             ChangePasswordButton.SetActive(false);
         }
-        getDBdata(query);
+
+        StartCoroutine(getDBdata((List<string> data) =>
+        {
+            userIDDisplay.text = data[0];
+            username.text = data[2];
+            level.text = data[1];
+            program.text = data[3];
+        }));
     }
 
     private void Awake()
@@ -76,15 +88,31 @@ public class SettingsManager : MonoBehaviour
     /// <summary>
     /// Function for calling values from the database.
     /// </summary>
-    private IEnumerator getDBdata(FirebaseUser query)
+    private IEnumerator getDBdata(Action<List<string>> onCallBack)
     {
-        var value = db.Child("Users").Child(query.Email).GetValueAsync();
-        userIDDisplay.text = value.Result.Value.ToString();
+        List<string> userData = new List<string>();
 
-        DataSnapshot item = value.Result;
-        level.text = item.Child("Level").Value.ToString();
-
+        var value = db.Child("users").Child(Utilities.removeDot(query.Email)).GetValueAsync();
         yield return new WaitUntil(predicate: () => value.IsCompleted);
+
+        if(value != null)
+        {
+            DataSnapshot item = value.Result;
+            userData.Add(item.Child("email").Value.ToString());
+            userData.Add(item.Child("level").Value.ToString());
+            userData.Add(item.Child("nickName").Value.ToString());
+            userData.Add(item.Child("program").Value.ToString());
+        }
+        else
+        {
+            // TODO: make this better?
+            // Handles case where no data was retrieved
+            userData.Add("Email Placeholder");
+            userData.Add("Level Placeholder");
+            userData.Add("Username Placeholder");
+            userData.Add("Program Placeholder");
+        }
+        onCallBack.Invoke(userData);
     }
 
     /// <summary>
@@ -92,7 +120,11 @@ public class SettingsManager : MonoBehaviour
     /// </summary>
     private void updateDBdata()
     {
-
+        // TODO: implement safety feature!
+        string emailWithoutDot = Utilities.removeDot(auth.CurrentUser.Email);
+        db.Child("users").Child(emailWithoutDot).Child("level").SetValueAsync(newLevel.text);
+        db.Child("users").Child(emailWithoutDot).Child("nickName").SetValueAsync(newUsername.text);
+        db.Child("users").Child(emailWithoutDot).Child("program").SetValueAsync(newProgram.text);
     }
 
     /// <summary>
@@ -113,20 +145,20 @@ public class SettingsManager : MonoBehaviour
     {
         FirebaseUser user = auth.CurrentUser;
 
-        // This checks that the current password
+        // This checks the current password
         user.ReauthenticateAsync(EmailAuthProvider.GetCredential(user.Email, CurrentPassword.text)).ContinueWith(task =>
         {
             Debug.LogFormat("new password attempt!");
             // If the current password is correct:
             if (task.IsCompletedSuccessfully)
             {
-                Debug.LogFormat("Password correct!");
+            Debug.LogFormat("Password correct!");
                 // If the new passwords match:
                 if (NewPassword.text == ConfirmPassword.text)
                 {
-                    Debug.LogFormat("Password matches!");
+                Debug.LogFormat("Password matches!");
                     // This attempts to update the password
-                    user.UpdatePasswordAsync(NewPassword.text).ContinueWith(update => 
+                    user.UpdatePasswordAsync(NewPassword.text).ContinueWith(update =>
                     {
                         if (update.IsCompletedSuccessfully)
                         {
@@ -137,11 +169,10 @@ public class SettingsManager : MonoBehaviour
                         {
                             Debug.LogFormat(update.Exception.ToString());
                             Debug.LogFormat("new password update failed!");
-                            // There is a chance insecure password is used, firebase will reject that password
-                            status.text = "New password update failed! \n" +
+                                // There is a chance insecure password is used, firebase will reject that password
+                                status.text = "New password update failed! \n" +
                                           "This password may not be viable!";
                         }
-                        return;
                     });
                 }
                 else
@@ -153,8 +184,53 @@ public class SettingsManager : MonoBehaviour
             {
                 status.text = "Current password is incorrect!";
             }
-            return;
+
+            new WaitUntil(predicate: () => task.IsCompleted);
         });
+
+        // Need to fix this, this method must be coroutined to work!
+        status.ForceMeshUpdate(true);
+
+        // Leaving the IEnumerator code here just in case!
+        //Debug.LogFormat("new password attempt!");
+        //// This checks the current password
+        //Task userValidation = user.ReauthenticateAsync(EmailAuthProvider.GetCredential(user.Email, CurrentPassword.text));
+        //yield return new WaitUntil(predicate: () => userValidation.IsCompleted);
+
+        //if (userValidation.IsCompletedSuccessfully)
+        //{
+        //    Debug.LogFormat("Password correct!");
+        //    // If the new passwords match:
+        //    if (NewPassword.text == ConfirmPassword.text)
+        //    {
+        //        Debug.LogFormat("Password matches!");
+
+        //        Task updatePassword = user.UpdatePasswordAsync(NewPassword.text);
+        //        yield return new WaitUntil(predicate: () => updatePassword.IsCompleted);
+
+        //        if (updatePassword.IsCompletedSuccessfully)
+        //        {
+        //            Debug.LogFormat("new password updated!");
+        //            status.text = "New password update successful!";
+        //        }
+        //        else
+        //        {
+        //            Debug.LogFormat(updatePassword.Exception.ToString());
+        //            Debug.LogFormat("new password update failed!");
+        //            // There is a chance insecure password is used, firebase will reject that password
+        //            status.text = "New password update failed! \n" +
+        //                          "This password may not be viable!";
+        //        }
+        //    }
+        //    else
+        //    {
+        //        status.text = "New password does not match!";
+        //    }
+        //}
+        //else
+        //{
+        //    status.text = "Current password is incorrect!";
+        //}
     }
 
     /// <summary>
@@ -162,7 +238,7 @@ public class SettingsManager : MonoBehaviour
     /// </summary>
     public void Return()
     {
-        SceneManager.LoadScene("MenuScene");
+    SceneManager.LoadScene("MenuScene");
     }
 
     /// <summary>
@@ -171,8 +247,10 @@ public class SettingsManager : MonoBehaviour
     public void Edit()
     {
         // All settings values should have the original values
+        userIDEdit.text = userIDDisplay.text;
         newUsername.text = username.text;
-
+        newProgram.text = program.text;
+        newLevel.text = level.text;
 
         DisplayCanvas.SetActive(false);
         EditCanvas.SetActive(true);
@@ -193,6 +271,10 @@ public class SettingsManager : MonoBehaviour
     public void Save()
     {
         updateDBdata();
+
+        username.text = newUsername.text;
+        level.text = newLevel.text;
+        program.text = newProgram.text;
 
         DisplayCanvas.SetActive(true);
         EditCanvas.SetActive(false);
