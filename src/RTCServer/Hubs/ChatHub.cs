c7c48d89bb7de8ac025b42934ffce40a8e54517b
Waseef Nayeem <nayeemw@mcmaster.ca>
2024-02-01 17:03:34 -0500

    feat(#146, #147): Added backend server

---
+++ src/RTCServer/Hubs/ChatHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace RTCServer.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(Message message)
+        {
+            Console.WriteLine($"Received: {message.Content}");
+            await Clients.Others.SendAsync("ReceiveMessage", message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            Console.WriteLine($"{Context.ConnectionId} has joined.");
+            var message = new Message { Content = $"{Context.ConnectionId} has joined." };
+            await Clients.Others.SendAsync("ReceiveMessage", message);
+        }
+    }
+
+    [System.Serializable]
+    public class Message
+    {
+        private string content = "";
+        public string Content
+        {
+            get => content;
+            set => content = value;
+        }
+    }
+}
