4beceadbc259949f95546174735ec777d198d0e0
Waseef Nayeem <nayeemw@mcmaster.ca>
2024-03-12 13:50:05 -0400

    chores: Fixed some formatting

--- src/CampusConnections/Assets/Scripts/ChatScene/ChatHelper.cs
+++ src/CampusConnections/Assets/Scripts/ChatScene/ChatHelper.cs
@@ -6,57 +6,51 @@ using UnityEngine.SceneManagement;
 
 public class ChatHelper : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject titleObj;
+    [SerializeField] private GameObject titleObj;
     private TMP_Text _title;
-    
-    [SerializeField]
-    private GameObject rtcObject;
+
+    [SerializeField] private GameObject rtcObject;
     private SignalRService _rtcService;
 
-    [SerializeField]
-    private GameObject inputFieldObj;
+    [SerializeField] private GameObject inputFieldObj;
     private InputFieldHandler _inputFieldHandler;
 
-    [SerializeField]
-    private GameObject msgPanelObj;
+    [SerializeField] private GameObject msgPanelObj;
 
-    [SerializeField]
-    private GameObject remoteMsgPrefab;
-    
-    [SerializeField]
-    private GameObject localMsgPrefab;
-    
+    [SerializeField] private GameObject remoteMsgPrefab;
+
+    [SerializeField] private GameObject localMsgPrefab;
+
     public List<GameObject> msgObjList;
 
     private Queue<string> _remoteMessageQueue;
 
-    void Awake()
+    private void Awake()
     {
         _title = titleObj.GetComponent<TMP_Text>();
         _rtcService = rtcObject.GetComponent<SignalRService>();
         _inputFieldHandler = inputFieldObj.GetComponent<InputFieldHandler>();
-        
+
         msgObjList = new List<GameObject>();
         _remoteMessageQueue = new Queue<string>();
     }
 
-    void Start()
+    private void Start()
     {
         var latestName = PlayerPrefs.GetString("LatestChatFriend");
         Debug.Log(latestName);
         _title.text = $"Chat with {latestName}";
-        
+
         _rtcService.NewMessageReceived.AddListener(OnReceivedListener);
         _inputFieldHandler.MessageSent.AddListener(OnSentListener);
     }
 
-    void Update()
+    private void Update()
     {
         while (_remoteMessageQueue.Count > 0)
         {
             var msg = _remoteMessageQueue.Dequeue();
-            
+
             CreateChatBubble(remoteMsgPrefab, msg);
         }
     }
