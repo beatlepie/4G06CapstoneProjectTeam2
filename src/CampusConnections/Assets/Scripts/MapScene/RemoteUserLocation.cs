05687de74346caf7d6dbdea649d24b8d9c8461d0
Waseef Nayeem <nayeemw@mcmaster.ca>
2024-03-15 13:21:12 -0400

    feat(#147): Added real-time multi-user map

---
+++ src/CampusConnections/Assets/Scripts/MapScene/RemoteUserLocation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class RemoteUserLocation
+{
+    private string email = "";
+    private double lat;
+    private double lng;
+
+    public double Latitude
+    {
+        get => lat;
+        set => lat = value;
+    }
+
+    public double Longitude
+    {
+        get => lng;
+        set => lng = value;
+    }
+
+    public string Email
+    {
+        get => email;
+        set => email = value;
+    }
+}
